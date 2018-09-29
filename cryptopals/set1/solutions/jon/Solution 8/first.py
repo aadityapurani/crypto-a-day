7a6fe7c5b7a234ecdebe3d5dc35d45a4b6485e97
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-09-29 11:30:25 -0500

    yes

--- cryptopals/set1/solutions/jon/Solution 8/first.py
+++ cryptopals/set1/solutions/jon/Solution 8/first.py
@@ -2,4 +2,11 @@ def solution():
 	f=open("code.txt")
 	code = f.read()
 	f.close()
-	return len(code.split('\n'));
+	return len(code.split('\n'))
+
+def splitString(str1,split_by= 2):
+	#str1 = strl[j:j+i]
+	#str2 = strl[j+i+1:j+i*2+1]
+	return [str1[i:i+split_by] for i in range(0,len(str1),split_by)]
+	
+	
