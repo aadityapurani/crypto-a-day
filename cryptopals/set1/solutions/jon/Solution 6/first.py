3af8454d06c70405b2f8c35075fc3b26323bc8f5
Jonathon <jon.kirkpatrick@sbcglobal.net>
2018-09-28 19:30:58 -0500

    f

--- cryptopals/set1/solutions/jon/Solution 6/first.py
+++ cryptopals/set1/solutions/jon/Solution 6/first.py
@@ -7,8 +7,8 @@ def solution():
 	bin_string1,bin_string2=convertToEvenBinaryString(string1,string2)
 	#bin_string1 = convertToBinaryString(string1)
 	#bin_string2 = convertToBinaryString(string2)
-	return edit_distance(bin_string1,bin_string2)
-
+	#return editDistDP(bin_string1,bin_string2,len(bin_string1),len(bin_string2))
+	return hammingDistance(bin_string1,bin_string2)
 #calculates the hamming distance
 
 def convertToBinaryString(string1):
@@ -70,12 +70,34 @@ def levenshteinDistance(s1, s2):
         distances = distances_
     return distances[-1]
 	
-def edit_distance(s1,s2):
-	l1,l2 = len(s1),len(s2)
-	arr = [[0 for i in range(l1+1)] for j in range(l2+1)]
-	for i in range(l2+1):
-		arr[i][0] = i
-	for j in range(l1+1):
-		arr[0][j] = j
-	
-	return arr
+def editDistDP(str1, str2, m, n): 
+    # Create a table to store results of subproblems 
+    dp = [[0 for x in range(n+1)] for x in range(m+1)] 
+  
+    # Fill d[][] in bottom up manner 
+    for i in range(m+1): 
+        for j in range(n+1): 
+  
+            # If first string is empty, only option is to 
+            # isnert all characters of second string 
+            if i == 0: 
+                dp[i][j] = j    # Min. operations = j 
+  
+            # If second string is empty, only option is to 
+            # remove all characters of second string 
+            elif j == 0: 
+                dp[i][j] = i    # Min. operations = i 
+  
+            # If last characters are same, ignore last char 
+            # and recur for remaining string 
+            elif str1[i-1] == str2[j-1]: 
+                dp[i][j] = dp[i-1][j-1] 
+  
+            # If last character are different, consider all 
+            # possibilities and find minimum 
+            else: 
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert 
+                                   dp[i-1][j],        # Remove 
+                                   dp[i-1][j-1])    # Replace 
+  
+    return dp[m][n] 
