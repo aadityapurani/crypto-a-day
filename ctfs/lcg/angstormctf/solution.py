19c4f191d944715ac35ca68fdfd82cda47bd08aa
jsk1465 <jon.kirkpatrick@sbcglobal.net>
2018-10-15 16:02:55 -0500

    f

---
+++ ctfs/lcg/angstormctf/solution.py
@@ -0,0 +1,13 @@
+
+def lcg(a,sn,b,m):
+    return (a*sn + b )% m
+
+def main():
+    t = open('flag.png.enc').read()
+    t = [t[i:i+4] for i in range(0, len(t), 4)]
+    	
+
+
+
+if(__name__ == '__main__'):
+    main()
