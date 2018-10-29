e3da208bd5051e5a2835eb2ad905da91914d03c4
jsk1465 <jon.kirkpatrick@sbcglobal.net>
2018-10-29 17:02:06 -0500

    f

--- ctfs/ecb/hitcon/jon/get_into_txt.py
+++ ctfs/ecb/hitcon/jon/get_into_txt.py
@@ -1,3 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+url = 'http://13.115.255.46/'
+r = requests.get(url)
 file = "temp_file.txt"
 f = open(file,'w')
-f.
+data = r.text
+
+soup = BeautifulSoup(data,features="html.parser")
+links = soup.find_all('a')[1:]
+crypto_data = []
+
+SPLIT_VAL = "\x00"
+
+for link in links:
+    #print(link.get('href'))
+    href = str(link.get('href'))
+    title = str(link)[len(link.get('href'))+11:-4]
+    h_url = requests.get(url+href).url.encode('utf-8')
+    #crypto_data.append({'ref':href,'url':h_url})
+    f.write(title+SPLIT_VAL+href+SPLIT_VAL+h_url+'\n')
+    print(title)
+f.close()
--- ctfs/ecb/hitcon/jon/solve.py
+++ ctfs/ecb/hitcon/jon/solve.py
@@ -1,17 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
-url = 'http://13.115.255.46/'
-r = requests.get(url)
+file = "temp_file.txt"
+f = open(file,'r')
+all_data = []
+for line in f.readlines():
+    d = line.split("\x00")
+    dic = {'title': d[0],'ref':d[1][3:],'url':d[2]}
+    if(dic['title']=='down'):
+        continue
+    all_data.append(dic)
 
-data = r.text
-
-soup = BeautifulSoup(data,features="html.parser")
-links = soup.find_all('a')[1:]
-crypto_data = []
-for link in links:
-    #print(link.get('href'))
-    href = str(link.get('href'))
-    title = str(link)[len(link.get('href'))+11:-4]
-    h_url = requests.get(url+href).url.encode('utf-8')
-    crypto_data.append({'ref':href,'url':h_url})
-    print(href,h_url)
+#IT'S probably the title
+for data in all_data:
+    split_size = 8
+    title_len = len(data['title'])
+    ref_2 = len(data['ref'])/2
+    num_split = len(data['ref'])/(16)
+    url_len = len(data['url'])
+    print('title: '+str(title_len) + '  ref/2: ' + str(ref_2)+'  num_splits: '+str(num_split)+ '  num_theosplits: ' + str(title_len/split_size)+'  IDK: '+str(url_len/split_size))
+    assert title_len < ref_2
